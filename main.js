0d809ae1f09c955fed25355330236e27fc5767d4
Codechtech <careers.codechtech@gmail.com>
2024-11-12 15:37:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -41,3 +41,43 @@ function toggleCard(card) {
     card.classList.toggle('expanded'); // Toggle full width for the clicked card
 
 }
+
+/*swiper testimonials*/
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+   speed: 400,
+   spaceBetween: 30,
+   autoplay: {
+    delay: 3000,
+    disableOnInteraction: false
+   },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    grabCursor: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+    }
+  });
+
+  /*scroll up*/
+  const scrollUp = () => {
+    const scrollUpBtn = document.getElementById("scroll-up")
+    if(this.scrollY >=250){
+        scrollUpBtn.classList.remove("-bottom-1/2")
+        scrollUpBtn.classList.add("bottom-4")
+    } else {
+        scrollUpBtn.classList.add("-bottom-1/2")
+        scrollUpBtn.classList.remove("bottom-4")
+    }
+  }
+  window.addEventListener("scroll", scrollUp)
+  
